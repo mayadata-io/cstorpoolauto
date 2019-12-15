ff1cc830bdee1dc14bb3787f6aad7048b89cef6b
AmitKumarDas <amit.das@mayadata.io>
2019-12-15 12:04:43 +0530

    chore(deploy): add storage provisioner's deploy artifacts to demo
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- demo/basic/test.sh
+++ demo/basic/test.sh
@@ -9,13 +9,18 @@ cleanup() {
   echo "++ Clean up started"
   echo "--------------------------"
 
-  kubectl delete -f openebs-operator-1.5.0.yaml || true
-  kubectl delete -f cspc-operator.yaml || true
-
   kubectl delete -f ../../deploy/operator.yaml || true
   kubectl delete -f ../../deploy/rbac.yaml || true
   kubectl delete -f ../../deploy/crd.yaml || true
+
+  kubectl delete -f storage_crd.yaml || true
+  kubectl delete -f storage_rbac.yaml || true
+  kubectl delete -f storage_deployment.yaml || true
+
   kubectl delete -f ../../deploy/namespace.yaml || true
+  
+  kubectl delete -f openebs-operator-1.5.0.yaml || true
+  kubectl delete -f cspc-operator.yaml || true
   
   echo "--------------------------"
   echo "++ Clean up completed"
@@ -26,16 +31,24 @@ cleanup() {
 # Uncomment this if you want to run this script in debug mode
 #set -ex
 
-echo -e "\n++ Installing operator"
-kubectl apply -f ../../deploy/crd.yaml
+echo -e "\n++ Installing cstorpoolauto operator"
 kubectl apply -f ../../deploy/namespace.yaml
+kubectl apply -f ../../deploy/crd.yaml
 kubectl apply -f ../../deploy/rbac.yaml
 kubectl apply -f ../../deploy/operator.yaml
-echo -e "\n++ Installed operator successfully"
+echo -e "\n++ Installed cstorpoolauto operator successfully"
 
+echo -e "\n++ Installing storage-provisioner operator"
+curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/storage_crd.yaml
+curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/rbac.yaml > storage_rbac.yaml
+curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/deployment.yaml > storage_deployment.yaml
+kubectl apply -f storage_crd.yaml
+kubectl apply -f storage_rbac.yaml
+kubectl apply -f storage_deployment.yaml
+echo -e "\n++ Installed storage-provisioner operator successfully"
 
 echo -e "\n++ Applying openebs"
-curl https://openebs.github.io/charts/openebs-operator-1.5.0.yaml > openebs-operator-1.5.0.yaml
+curl https://openebs.github.io/charts/openebs-operator-1.5.0.yaml
 kubectl apply -f openebs-operator-1.5.0.yaml
 echo -e "\n++ Applied openebs successfully"
 
