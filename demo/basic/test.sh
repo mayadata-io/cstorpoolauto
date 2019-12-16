29908e5ea5369832f6bb5df38ebf088109519d71
AmitKumarDas <amit.das@mayadata.io>
2019-12-16 12:22:42 +0530

    fix(reconciler): throw errors for nil hook request or response
    This commit throws error if either hook request or hook response
    are nil. These objects are expected to be instantiated by metac.
    Reconciliation will not work as expected if any one or both of
    them are nil.
    
    This commit also has additional scripts, artifacts required to
    deploy or delete the entire cstorpoolauto setup.
    
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- demo/basic/test.sh
+++ demo/basic/test.sh
@@ -39,7 +39,7 @@ kubectl apply -f ../../deploy/operator.yaml
 echo -e "\n++ Installed cstorpoolauto operator successfully"
 
 echo -e "\n++ Installing storage-provisioner operator"
-curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/storage_crd.yaml
+curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/storage_crd.yaml > storage_crd.yaml
 curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/rbac.yaml > storage_rbac.yaml
 curl https://raw.githubusercontent.com/mayadata-io/storage-provisioner/master/deploy/kubernetes/deployment.yaml > storage_deployment.yaml
 kubectl apply -f storage_crd.yaml
@@ -48,12 +48,12 @@ kubectl apply -f storage_deployment.yaml
 echo -e "\n++ Installed storage-provisioner operator successfully"
 
 echo -e "\n++ Applying openebs"
-curl https://openebs.github.io/charts/openebs-operator-1.5.0.yaml
+curl https://openebs.github.io/charts/openebs-operator-1.5.0.yaml > openebs-operator-1.5.0.yaml
 kubectl apply -f openebs-operator-1.5.0.yaml
 echo -e "\n++ Applied openebs successfully"
 
 echo -e "\n++ Applying cspc operator"
-curl https://raw.githubusercontent.com/openebs/openebs/master/k8s/cspc-operator.yaml 
+curl https://raw.githubusercontent.com/openebs/openebs/master/k8s/cspc-operator.yaml > cspc-operator.yaml
 kubectl apply -f cspc-operator.yaml
 echo -e "\n++ Applied cspc operator successfully"
 
